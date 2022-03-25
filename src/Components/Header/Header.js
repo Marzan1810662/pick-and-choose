4d662af729e558a226cb16f4e04264aca1529800
Marzan1810662 <hassanmarzan17@gmail.com>
2022-03-26 00:39:19 +0600

    Header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h1>Pick & Choose</h1>
+            <h3>Pick A Random One From Any Four </h3>
+        </div>
+    );
+};
+
+export default Header;
