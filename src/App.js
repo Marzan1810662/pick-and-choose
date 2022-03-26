6233037c95004d55e7ab3954e90173ec6fe5b1fe
Marzan1810662 <hassanmarzan17@gmail.com>
2022-03-26 17:14:55 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Homepage from './Components/Homepage/Homepage';
+import QuestionAnswer from './Components/QuestionAnswer/QuestionAnswer';
 
 function App() {
   return (
     <div className='main-container'>
       <Header></Header>
       <Homepage></Homepage>
+      <QuestionAnswer></QuestionAnswer>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <p><small> © 2022 p&c. All rights reserved</small></p>
+        </div>
+    );
+};
+
+export default Footer;
